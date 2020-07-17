bf40dd9d767446f9c7c18b05071ce0658e3ade55
setyapi <setyapinandhita@mail.unpas.ac.id>
2020-07-17 16:14:38 +0700

    database barang_elektronik

--- Pertemuan 1/rekwebss2020_133040054.sql
+++ Pertemuan 1/rekwebss2020_133040054.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2020 at 10:35 AM
+-- Generation Time: Jul 17, 2020 at 11:04 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
